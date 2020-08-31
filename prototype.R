668b1567e8ff1c6fec5f2cdee71d5aa2ffb1b392
liangchenn <r08323022@ntu.edu.tw>
2020-08-31 15:42:17 +0800

    Add basic multinomial case

--- prototype.R
+++ prototype.R
@@ -20,3 +20,37 @@ mean(yhat)
 
 
 summary(m)
+
+
+
+# simple multinomial choice case ------------------------------------------
+obs <- 10000
+x1 <- rnorm(obs)
+x2 <- rnorm(obs)
+beta <- 2
+
+u0 <- -log(-log(runif(obs)))
+u1 <- x1*beta + -log(-log(runif(obs)))
+u2 <- x2*beta + -log(-log(runif(obs)))
+
+y <- case_when(
+  
+  u1 > u2 & u1 > u0 ~ 1,
+  
+  u2 > u1 & u2 > u0 ~ 2,
+  
+  u0 >= u1 & u0 >= u2 ~ 0
+)
+
+  
+# empirical market share
+table(y) %>% prop.table
+
+# analytic share
+yhat_1 <- exp(x1*beta) / ( exp(0) + exp(x1*beta) + exp(x2*beta) )
+yhat_2 <- exp(x2*beta) / ( exp(0) + exp(x1*beta) + exp(x2*beta) )
+yhat_0 <- exp(0) / ( exp(0) + exp(x1*beta) + exp(x2*beta) )
+
+mean(yhat_0)
+mean(yhat_1)
+mean(yhat_2)
